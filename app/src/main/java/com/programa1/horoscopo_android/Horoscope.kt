2a6685e975d9d371b3426f07739b9c58b447ddc7
CarlosGomezIglesias <carlos.gomez.igle@gmail.com>
2026-04-10 16:04:46 +0200

    Añadidos los strings de nombres y las iconos del zodiaco

---
+++ app/src/main/java/com/programa1/horoscopo_android/Horoscope.kt
@@ -0,0 +1,10 @@
+package com.programa1.horoscopo_android
+
+class Horoscope (
+    val id: String,
+    val name: Int,
+    val dates: Int,
+    val image: Int
+){
+
+}
--- app/src/main/java/com/programa1/horoscopo_android/MainActivity.kt
+++ app/src/main/java/com/programa1/horoscopo_android/MainActivity.kt
@@ -7,6 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    val horoscopeList: List<Horoscope> = listOf(
+        Horoscope("", R.string.aries_name, R.string.aries_date, R.drawable.aries_icon)
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
